aa16a82e7d24f9ead2f19cc435f64ac85ce29795
Kris <kristinaovchinnikova22@gmail.com>
2025-02-17 21:44:38 +0600

    Сделала регестриацую пользователя, вход по google-email, а так же вход, и выход пользователя

---
+++ backend-cocktail/config.ts
@@ -0,0 +1,15 @@
+import path from "path";
+
+const rootPath = __dirname;
+
+const config = {
+    rootPath,
+    publicPath: path.join(rootPath, 'public'),
+    google:{
+        clientId:process.env.GOOGLE_CLIENT_ID,
+        secretId: process.env.GOOGLE_CLIENT_SECRET,
+    },
+    db:'mongodb://localhost/cocktails'
+}
+
+export default config;
